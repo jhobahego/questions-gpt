f75ccc583abcfb3d199b2dbb040029a242c9e815
Jhon Hernandez <jhobahego16@hotmail.com>
2023-06-22 16:32:21 -0500

    Primer commit

---
+++ src/main/java/com/jhobahego/questionsgpt/QuestionsGptApplication.java
@@ -0,0 +1,13 @@
+package com.jhobahego.questionsgpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionsGptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuestionsGptApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jhobahego/questionsgpt/QuestionsGptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhobahego.questionsgpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionsGptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
