68106d5560c29a1fc2e75154f3601bd0ecf1aa10
Jhon Hernandez <jhobahego16@hotmail.com>
2023-06-30 20:39:25 -0500

    Feat: servicio generarPregunta

--- src/main/java/com/jhobahego/questionsgpt/servicios/ChatgtpService.java
+++ src/main/java/com/jhobahego/questionsgpt/servicios/ChatgtpService.java
@@ -1,29 +1,19 @@
 package com.jhobahego.questionsgpt.servicios;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 @Service
 public class ChatgtpService {
 
-    @Autowired
-    private Environment environment;
+    private final String OPEN_API_URL = "https://api.openai.com/v1/chat/completions";
 
-    private final String OPEN_API_URL = environment.getProperty("OPEN_API_URL");
-
-    private final String API_KEY = environment.getProperty("API_KEY");
+    private final String API_KEY = "your_api_key";
 
     public String generarPregunta(String prompt) throws IOException {
-
         URL apiUrl = new URL(OPEN_API_URL);
 
         HttpURLConnection connection = (HttpURLConnection) apiUrl.openConnection();
